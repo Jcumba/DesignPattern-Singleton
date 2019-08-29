6b71e1a8b88196ec3c57d3afd82dc512c2d8a0d7
José Carlos Cumba Gamboa <jcumba@vallegrande.edu.pe>
2019-08-29 12:47:44 -0500

    TEST

---
+++ Singleton/src/main/java/dao/App.java
@@ -0,0 +1,17 @@
+package dao;
+
+import modelo.Conexion;
+
+public class App {
+
+	public static void main(String[] args) {
+		//Instanciación por constructor prohíbido por ser "private"
+		//Conexion c = new Conexion();
+		Conexion c = Conexion.getInstancia();
+		c.conectar();
+		c.desconectar();
+		
+		boolean rpta = c instanceof Conexion;
+		System.out.println(rpta);
+	}
+}
